5a1e576c6982f2dad977712fb5d25d8354bda076
Elia Polloniato <polelia17@gmail.com>
2021-03-30 14:10:53 +0200

    first commit

---
+++ deployDocker.sh
@@ -0,0 +1,10 @@
+export PROJECT_NAME itis_discord_bot
+
+echo "Building itis_discord_bot..."
+docker buildx build --platform linux/arm64 --tag $PROJECT_NAME --load .
+
+echo "Updating image..."
+docker tag $(docker images --filter=reference=$PROJECT_NAME --format "{{.ID}}") 192.168.0.8:32768/$PROJECT_NAME:latest
+
+echo "Pushing image..."
+docker push 192.168.0.8:32768/$PROJECT_NAME:latest
